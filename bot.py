92acd94ef7b6913b21229ae42dd93afe321fec7f
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-01-25 21:17:09 +0100

    Ajout du support de plusieurs sites

--- bot.py
+++ bot.py
@@ -3,18 +3,19 @@ import time
 from bs4 import BeautifulSoup
 from variables import *
 
-
+find = False
 # Boucle infinie pour vérifier la page toutes les minutes
-while True:
+while find == False:
+    for nSite in range(len(sites)):
     # Récupération du code source de la page
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+        response = requests.get(sites[nSite]['url'])
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Vérification si l'élément est présent
-    element = soup.select_one(selector)
-    if element and text not in element.text:
-        # Envoi d'un message sur Discord si l'élément n'est pas présent
-        requests.post(webhook_url, json={'content': 'Le produit est maintenant disponible !'})
-        
-    else:
-        time.sleep(60)
+        # Vérification si l'élément est présent
+        element = soup.select_one(sites[nSite]['selector'])
+        if element and sites[nSite]['text'] not in element.text:
+            # Envoi d'un message sur Discord si l'élément n'est pas présent
+            requests.post(webhook_url, json={'content': 'Le produit est maintenant disponible ! sur le site : ' + sites[nSite]['url']})
+            find = True
+        else:
+            time.sleep(60)
--- variables.py
+++ variables.py
@@ -1,11 +1,9 @@
-# URL de la page web à vérifier
-url = 'https://www.kubii.fr/cartes-raspberry-pi/2955-raspberry-pi-4-modele-b-8gb-0765756931199.html'
+sites = [
+    {'url' : 'https://www.kubii.fr/cartes-raspberry-pi/2955-raspberry-pi-4-modele-b-8gb-0765756931199.html', 'selector' : '#availability_value', 'text' : 'En rupture de stock'},
+    {'url' : 'https://shop.mchobby.be/fr/raspberry-pi-4/1609-raspberry-pi-4-2-go-de-ram-dispo-en-stock--3232100016095.html?src=raspberrypi', 'selector' : '.alert-danger', 'text' : 'Rupture de stock'},
+]
 
-# Élément à rechercher
-selector = '#availability_value'
 
-# Texte à rechercher
-text = 'En rupture de stock'
 
 # URL du webhook Discord
-webhook_url = 'url du webbook discord'
+webhook_url = 'URL du webhook Discord'
