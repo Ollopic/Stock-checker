e0f39e14535e72477f3e7e78917092c66adcbfe6
mare0055 <antoine.marechal@etudiant.univ-reims.fr>
2023-02-05 16:00:56 +0100

    Ajout d'un message discord en cas de non réponse d'un site

--- bot.py
+++ bot.py
@@ -7,20 +7,25 @@ find = False
 # Boucle infinie pour vérifier la page toutes les minutes
 while find == False:
     for nSite in range(len(sites)):
-    # Récupération du code source de la page
-        headers = {'User-Agent': 'Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'}
-        response = requests.get(sites[nSite]['url'], timeout=15, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+        # Récupération du code source de la page
+            headers = {'User-Agent': 'Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'}
+            response = requests.get(sites[nSite]['url'], timeout=200, headers=headers)
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Vérification si l'élément est présent
-        element = soup.select_one(sites[nSite]['selector'])
-        if element and sites[nSite]['text'] not in element.text:
-            # Envoi d'un message sur Discord si l'élément n'est pas présent (le produit est disponible)
-            requests.post(webhook_url, json={'content': 'Le produit est maintenant disponible ! sur le site : ' + sites[nSite]['url']})
-            find = True
-        if element == None:
-            # Envoi d'un message sur Discord informant que le selecteur CSS n'a pas été trouvé
-            requests.post(webhook_url, json={'content': 'Le selecteur CSS du site ' + sites[nSite]['url'] + ' n\'a pas été trouvé.'})
-            find = True
+            # Vérification si l'élément est présent
+            element = soup.select_one(sites[nSite]['selector'])
+            print(element)
+            if element and sites[nSite]['text'] not in element.text:
+                # Envoi d'un message sur Discord si l'élément n'est pas présent (le produit est disponible)
+                requests.post(webhook_url, json={'content': 'Le produit est maintenant disponible ! sur le site : ' + sites[nSite]['url']})
+                find = True
+            if element == None:
+                # Envoi d'un message sur Discord informant que le selecteur CSS n'a pas été trouvé
+                requests.post(webhook_url, json={'content': 'Le selecteur CSS du site ' + sites[nSite]['url'] + ' n\'a pas été trouvé.'})
+                find = True
+        except requests.exceptions.Timeout:
+            # Envoi d'un message sur Discord informant que le site a mis trop de temps à répondre
+            requests.post(webhook_url, json={'content': 'Le site ' + sites[nSite]['url'] + ' a mis trop de temps à répondre.'})
 
     time.sleep(50)
